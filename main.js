0887a1567621b8608ff26ddd7cd1d9ff22a5dbd5
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2023-10-30 14:53:10 -0600

    awd

--- main.js
+++ main.js
@@ -7,116 +7,159 @@ cnv.height = 550;
 // EVENT STUFF
 
 // Reset Variables
-let circles = [];
-let rectangles = [];
+let circles;
+let rectangles;
+let mouseIsPressed = false;
+let mouseX;
+let mouseY;
+let gameState = "play";
 
 reset();
+
+// Event Stuff
+document.addEventListener("mousemove", mousemoveHandler);
+document.addEventListener("mouseup", mouseupHandler);
+
+function mousemoveHandler(e) {
+  // Get rectangle info about canvas location
+  let cnvRect = cnv.getBoundingClientRect();
+
+  // Calc mouse coordinates using mouse event and canvas location info
+  mouseX = Math.round(e.clientX - cnvRect.left);
+  mouseY = Math.round(e.clientY - cnvRect.top);
+}
+
+function mousedownHandler(event) {
+    mouseIsPressed = true;
+}
+
+function mouseupHandler(event) {
+    mouseIsPressed = false;
+}
 
 // Animation
 requestAnimationFrame(animate);
 function animate() {
     ctx.fillStyle = `rgb(50, 50, 50)`;
     ctx.fillRect(0, 0, cnv.width, cnv. height);
-
-    for (let i = 0; i < walls.length; i++) {
-        drawWalls(i);
-        checkCollision(i);
+    
+    for (let i = 0; i < rectangles.length; i++) {
+        drawRectangles(i);
+        shapeMovement(rectangles, i);
+        if (mouseIsPressed === true) {
+            shapeClicked(rectangles, i)
+        }
     }
 
-    drawPlayer();
-    playerMovement();
+    for (let i = 0; i < circles.length; i++) {
+        drawCircle(i);
+        shapeMovement(circles, i);
+        if (mouseIsPressed === true) {
+            shapeClicked(circles, i)
+        }
+    }
+
+    if (gameState === "gameOver") {
+        if (circles.length === 0) {
+            alert("Awesome");
+        } else {
+            alert("try again");
+        }
+    }
 
     // Request Animation Frame
     requestAnimationFrame(animate);
 }
 
-function drawWalls(n) {
-    ctx.fillStyle = `${walls[n].color}`;
-    ctx.fillRect(walls[n].x, walls[n].y, walls[n].w, walls[n].h);
+function drawRectangles(n) {
+    ctx.strokeStyle = rectangles[n].color;
+    ctx.lineWidth = rectangles[n].lineWidth;
+    ctx.strokeRect(rectangles[n].x, rectangles[n].y, rectangles[n].w, rectangles[n].h);
 }
 
-function drawPlayer() {
-    ctx.fillStyle = `${player[0].color}`;
-    ctx.fillRect(player[0].x, player[0].y, player[0].w, player[0].h);
+function drawCircle(n) {
+    ctx.strokeStyle = circles[n].color;
+    ctx.lineWidth = circles[n].lineWidth;
+    ctx.beginPath();
+    ctx.arc(circles[n].x, circles[n].y, circles[n].r, circles[n].startAngle, circles[n].endAngle * Math.PI);
+    ctx.stroke();
 }
 
-function playerMovement() {
-    if (player[0].up === true) {
-        player[0].y -= player[0].v;
-    }
-    if (player[0].left === true) {
-        player[0].x -= player[0].v;
-    }
-    if (player[0].right === true) {
-        player[0].x += player[0].v;
-    }
-    if (player[0].down === true) {
-        player[0].y += player[0].v;
-    }
-}
+function shapeMovement(shape, n) {
+    shape[n].x += shape[n].xVelocity;
+    shape[n].y += shape[n].yVelocity;
 
-function checkCollision(n) {
-    // Top Detection
-    if (player[0].y < walls[n].y + walls[n].h && player[0].y > walls[n].y && player[0].x + player[0].v < walls[n].x + walls[n].w && player[0].x + player[0].w - player[0].v > walls[n].x) {
-        player[0].y = walls[n].y + walls[n].h;
+    if (shape === rectangles) {
+        if (shape[n].x > cnv.width + 5) {
+            shape[n].x = -shape[n].w;
+        } else if (shape[n].x + shape[n].w < -5) {
+            shape[n].x = cnv.width;
+        }
+        if (shape[n].y > cnv.height + 5) {
+            shape[n].y = -shape[n].h;
+        } else if (shape[n].y + shape[n].w < -5) {
+            shape[n].y = cnv.height;
+        }
     }
-    // Bottom Detection
-    if (player[0].y + player[0].h > walls[n].y && player[0].y + player[0].h < walls[n].y + walls[n].h && player[0].x + player[0].v < walls[n].x + walls[n].w && player[0].x + player[0].w - player[0].v > walls[n].x) {
-        player[0].y = walls[n].y - player[0].h;
-    }
-    // Left Detection
-    if (player[0].x < walls[n]. x + walls[n].w && player[0].x > walls[n].x && player[0].y < walls[n].y + walls[n].h && player[0].y + player[0].h > walls[n].y) {
-        player[0].x = walls[n].x + walls[n].w;
-    }
-    // Right Detection
-    if (player[0].x + player[0].w > walls[n]. x && player[0].x + player[0].w  < walls[n].x + walls[n].w && player[0].y < walls[n].y + walls[n].h && player[0].y + player[0].h > walls[n].y) {
-        player[0].x = walls[n].x - player[0].w;
+
+    if (shape === circles) {
+        if (shape[n].x > cnv.width || shape[n].x < 0) {
+            shape[n].xVelocity = shape[n].xVelocity * -1;
+        }
+        if (shape[n].y > cnv.height || shape[n].y < 0) {
+            shape[n].yVelocity = shape[n].yVelocity * -1;
+        }
     }
 }
 
-function newPlayer(x1, y1, w1, h1, v1, color1, up1, left1, right1, down1) {
+function shapeClicked(shape, n) {
+    if (shape === rectangles) {
+        if (mouseX > shape[n].x && mouseX < shape[n].x + shape[n].w && mouseY > rectangles[n].y && mouseY < rectangles[n].y + rectangles[n].h) {
+            gameState = "gameOver";
+        }
+    } else if (shape === circles) {
+        if (Math.sqrt(Math.pow((mouseX - shape[n].x), 2) + Math.pow((mouseY - shape[n].y), 2)) <= shape[n].r) {
+            shape.splice(n, 1);
+            console.log(true);
+          }
+    }
+}
+
+function newCircle(x1, y1, r1, lineWidth1, startAngle1, endAngle1, xVelocity1, yVelocity1, color1) {
     return {
             x: x1,
             y: y1,
-            w: w1,
-            h: h1,
-            v: v1,
+            r: r1,
+            lineWidth: lineWidth1,
+            startAngle: startAngle1,
+            endAngle: endAngle1,
+            xVelocity: xVelocity1,
+            yVelocity: yVelocity1,
             color: color1,
-            up: up1,
-            left: left1,
-            right: right1,
-            down: down1
-        } 
+        };
 }
 
-function newWall(x1, y1, w1, h1, color1) {
+function newRectangle(x1, y1, w1, h1, lineWidth1, xVelocity1, yVelocity1, color1) {
     return {
             x: x1,
             y: y1,
             w: w1,
             h: h1,
+            lineWidth: lineWidth1,
+            xVelocity: xVelocity1,
+            yVelocity: yVelocity1,
             color: color1,
         }
 }
 
 function reset() {
-    walls = [];
-    walls.push(newWall(0, 0, cnv.width, 20, "grey"));
-    walls.push(newWall(0, 0, 20, cnv.height, "grey"));
-    walls.push(newWall(cnv.width - 20, 0, 20, cnv.height, "grey"));
-    walls.push(newWall(0, cnv.height - 20, cnv.width, 20, "grey"));
-    walls.push(newWall(0, cnv.height - 20, cnv.width, 20, "grey"));
-    walls.push(newWall(125, 175, 100, 20, "grey"));
-    walls.push(newWall(500, 20, 20, 100, "grey"));
-    walls.push(newWall(300, 100, 20, 225, "grey"));
-    walls.push(newWall(400, 150, 150, 20, "grey"));
-    walls.push(newWall(450, 250, 20, 150, "grey"));
-    walls.push(newWall(600, 200, 150, 20, "grey"));
-    walls.push(newWall(200, 325, 20, 175, "grey"));
-    walls.push(newWall(600, 300, 20, 150, "grey"));
-    walls.push(newWall(250, 400, 150, 20, "grey"));
-    walls.push(newWall(380, 450, 20, 100, "grey"));
+    rectangles = [];
+    for (let i = 0; i < 10; i++) {
+        rectangles.push(newRectangle(randomInt(0, cnv.width), randomInt(0, cnv.height), randomInt(25, 50), randomInt(25, 50), 3, randomInt(-5, 5), randomInt(-5, 5), "red"));
+    }
 
-    player = [];
-    player.push(newPlayer(50, cnv.height / 2, 20, 20, 5, "blue", false, false, false, false,));
+    circles = [];
+    for (let i = 0; i < 15; i++) {
+        circles.push(newCircle(randomInt(0, cnv.width), randomInt(0, cnv.height), randomInt(15, 30), 3, 0, 2, randomInt(-5, 5), randomInt(-5, 5), "green"));
+    }
 }
